f55d99f1011169c13693ba6f318b1695ba2bf573
MAC SMS PRO <89928052+Macsmspro@users.noreply.github.com>
2021-09-01 16:40:37 +0100

    Create sms.php

---
+++ sms.php
@@ -0,0 +1,38 @@
+ <?php
+
+$url = "https://macsmspro.com/api/otp.php";
+$fields = array(
+    
+    "name" => urlencode("xxxxxx"), // à remplacer par le nom du message
+    "telephone" => urlencode("229xxxxxxxx"), // à remplacer par le numéro du destinataire
+    "message" => urlencode("Mon message"), // à remplacer par le message à envoyer
+    "token" => urlencode("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"), // à remplacer par votre token 
+);
+
+$fields_string = "";
+foreach($fields as $key=>$value) { $fields_string .= $key."=".$value."&"; }
+
+
+rtrim($fields_string, "&");
+
+$ch = curl_init();
+
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+
+
+$result = json_decode($result);
+
+print_r($result);
+
+
+
+?>
